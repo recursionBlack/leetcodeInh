b3730dd400be86f2cfce0b346849e6cbfbb0f51a
taishiji <tsj@tecwin.com>
2023-09-09 09:21:20 +0800

    添加322题

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "../Qstring/lec013.h"
 #include "../NodeList/MyList.h"
 #include "../NodeList/lec021.h"
+#include "../DP/lec322.h"
 
 using namespace std;
 
 int main()
 {
-    Solution021 *S = new Solution021;
+    Solution322 *S = new Solution322;
     S->ShuRu();
-    S->ChuShu();
+    S->CalcResult();
     system("pause");
     return 0;
 }
