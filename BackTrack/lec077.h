a0f75f1d916d2889e8408733ba30a0358973e789
Kuan <roumiankuan@outlook.com>
2025-08-04 15:46:36 +0800

    修改77题

--- BackTrack/lec077.h
+++ BackTrack/lec077.h
@@ -4,66 +4,32 @@
 #include <iostream>
 
 using namespace std;
-
-class Solution077 {
-	//组合问题，数学中的Cnk
-	vector<vector<int>> res;
+//组合问题，数学中的Cnk
+class Solution {
 public:
 	vector<vector<int>> combine(int n, int k) {
-		vector<int> pat;
-
-		backtrack(1, n, k, pat);
-		return res;
+		vector<vector<int>> path;
+		vector<int> tmp;
+		backtrack(n, k, 1, tmp, path);
+		
+		return path;
 	}
 
-	// curstar是当前起始参数，
-	void backtrack(int curstar, int n, int k, vector<int> path)
+	// begin是当前起始参数，
+	void backtrack(int n, int k, int begin, vector<int>& tmp, vector<vector<int>>& path)
 	{
-		if (k == path.size())
+		if (k == 0)
 		{
-			res.push_back(path);
+			path.push_back(tmp);
 			return;
 		}
 		
-		// 这个题，神奇就神奇在i = curstar， 与backtrack函数的第一个参数上
-		for (int i = curstar; i <= n; i++)
+		// 这个题，神奇就神奇在i = begin， 与backtrack函数的第一个参数上
+		for (size_t i = begin; i <= n; i++)
 		{
-			path.push_back(i);
-			backtrack(i + 1, n, k, path);  // 
-			path.pop_back();
+			tmp.push_back(i);
+			backtrack(n, k - 1, i + 1, tmp, path);
+			tmp.pop_back();
 		}
 	}
-	//	输入函数
-	void ShuRu()
-	{
-		cout << "please putin n" << endl;
-		cin >> m_N;
-		cin.clear();
-		cin.ignore();
-
-		cout << "please putin k" << endl;
-		cin >> m_K;
-		cin.clear();
-		cin.ignore();
-
-	}
-	// 输出函数
-	void CalcResult()
-	{
-		vector<vector<int>> res = combine(m_N, m_K);
-		for (auto s : res)
-		{
-				for (int c:s)
-				{
-					cout << c << " ";
-				}
-				cout << endl;
-		}
-	}
-
-public:
-
-	int m_N = 0;
-	int m_K = 0;
-
 };
