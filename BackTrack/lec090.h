8c6cd47921e7a55811d998bbe4e842db77fc1be3
Kuan <roumiankuan@outlook.com>
2025-08-04 17:20:55 +0800

    修改90

--- BackTrack/lec090.h
+++ BackTrack/lec090.h
@@ -6,31 +6,29 @@
 
 using namespace std;
 
-// 子集问题
-class Solution090 {
-	vector<vector<int>> vec;
+// 子集问题2
+class Solution {
+	
 public:
 	vector<vector<int>> subsetsWithDup(vector<int>& nums) {
 		sort(nums.begin(), nums.end());
-		vector<bool> used(nums.size(), false);
-		vector<int> pat;
-		backtrack(nums, used, 0, pat);
-		return vec;
+		vector<vector<int>> path;
+		vector<int> tmp;
+		backtrack(nums, 0, tmp, path);
+		return path;
 	}
 
 	// 相比于78题，只多了个同层剪枝的操作
-	void backtrack(vector<int>& nums, vector<bool> used, int starindex, vector<int> path)
+	void backtrack(vector<int>& nums,  int starindex, vector<int>& tmp, vector<vector<int>>& path)
 	{
-		vec.push_back(path);
-		for (int i = starindex; i < nums.size(); i++)
+		path.push_back(tmp);
+		for (size_t i = starindex; i < nums.size(); i++)
 		{
-			if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])     // 同层剪枝
+			if (i > starindex && nums[i] == nums[i - 1])     // 同层剪枝
 				continue;
-			path.push_back(nums[i]);
-			used[i] = true;
-			backtrack(nums, used, i + 1, path);
-			used[i] = false;
-			path.pop_back();
+			tmp.push_back(nums[i]);
+			backtrack(nums, i + 1, tmp, path);
+			tmp.pop_back();
 		}
 	}
 };
