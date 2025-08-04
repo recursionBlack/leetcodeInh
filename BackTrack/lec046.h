bdda70f88006f1744b018a36b43bb9785ec4b0c6
Kuan <roumiankuan@outlook.com>
2025-08-04 11:09:13 +0800

    46题修改参数，并添加测试用例

--- BackTrack/lec046.h
+++ BackTrack/lec046.h
@@ -1,36 +1,81 @@
 #include <vector>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-class Solution046 {
-	vector<vector<int>> vec;
+class Solution {
+	
 public:
 	vector<vector<int>> permute(vector<int>& nums) {
-		if (nums.size() == 0)
+		if (nums.empty())
 		{
 			return { {} };
 		}
-		vec.clear();
+		sort(nums.begin(), nums.end());
 		int n = 0;
-		vector<int> pat;
-		backtrack(nums, n, pat);
-		return vec;
+		vector<vector<int>> path;
+		vector<int> tmp;
+		backtrack(nums, n, tmp, path);
+
+		return path;
 
 	}
-	void backtrack(vector<int>& nums, int n, vector<int> path)
+	void backtrack(vector<int>& nums, int n, vector<int>& tmp, vector<vector<int>>& path)
 	{
 		if (n == nums.size())
 		{
-			vec.push_back(path);
+			path.push_back(tmp);
 			return;
 		}
-		for (int i = 0; i < nums.size(); i++)
+		for (size_t i = 0; i < nums.size(); i++)
 		{
-			if (find(path.begin(), path.end(), nums[i]) != path.end())
-				continue;
-			path.push_back(nums[i]);    // 做选择
-			backtrack(nums, n + 1, path);         // dfs
-			path.pop_back();            //取消选择，回溯算法与dfs区别的最重要标志，
+			// 该数字已经用过了, 就不再使用了
+            if(find(tmp.begin(),tmp.end(), nums[i]) != tmp.end())
+            {
+                continue;
+            }
+
+			tmp.push_back(nums[i]);    // 做选择
+			backtrack(nums, n + 1, tmp, path);         // dfs
+			tmp.pop_back();            //取消选择，回溯算法与dfs区别的最重要标志，
 		}
 	}
 };
+
+
+// 打印二维向量
+void printVector(const vector<vector<int>>& vec) {
+    cout << "[" << endl;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        cout << "  [";
+        for (size_t j = 0; j < vec[i].size(); ++j) {
+            cout << vec[i][j];
+            if (j != vec[i].size() - 1) {
+                cout << ",";
+            }
+        }
+        cout << "]";
+        if (i != vec.size() - 1) {
+            cout << ",";
+        }
+        cout << endl;
+    }
+    cout << "]" << endl;
+}
+
+// int main() {
+//     Solution sol;
+//     vector<int> nums = {1, 2, 3};
+    
+//     // 函数输出
+//     vector<vector<int>> result = sol.permute(nums);
+    
+//     // 打印信息
+//     cout << "测试输入: nums = [1,2,3]" << endl;
+// 	cout << "预期输出: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]" << endl;
+//     cout << "实际输出:" << endl;
+//     printVector(result);
+    
+//     return 0;
+// }
