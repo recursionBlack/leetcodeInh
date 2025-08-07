7a391c704bdc1ed6e64775b936732b50186bee01
Kuan <roumiankuan@outlook.com>
2025-08-07 08:24:06 +0800

    此后，除了复习已经写在这里的题目外，不再使用cpp来刷算法题了，main函数也可以归档了

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
-#include "MyList.h"
-#include "lec021.h"
-#include "lec329_2.h"
+#include <iostream>
+#include "lec078.h"
 
 using namespace std;
 
 int main()
 {
-    Solution329_2*S = new Solution329_2;
-    S->ShuRu();
-    S->CalcResult();
-    system("pause");
+    Solution sol;
+    vector<int> nums = {1, 2, 3};
+    
+    // 函数输出
+    vector<vector<int>> result = sol.subsets(nums);
+    
+    // 打印信息
+    cout << "测试输入: nums = [1,2,3]" << endl;
+    cout << "预期输出（所有子集）: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]" << endl;
+    cout << "实际输出:" << endl;
+    printSubsets(result);
+
     return 0;
 }
